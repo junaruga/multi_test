d140c4950129a4a689526bc7083c2b0500b8b111
Jun Aruga <happy@oh4u.net>
2016-01-15 18:55:10 +0100

    Update multi_proc_main

--- src/multi_proc_main.c
+++ src/multi_proc_main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int do_something(int shm_id)
@@ -28,6 +29,8 @@ int do_something(int shm_id)
         return ERR;
     }
 
+    int pid = getpid();
+    srand(pid);
     int r = rand();
     int sleeping_sec = r % 10;
 
@@ -43,7 +46,8 @@ int main(int argc, const char **argv)
 {
     int shm_id = 0;
     int i = 0;
-    pid_t pid = 0;
+    pid_t pid;
+    int status = OK;
 
     if ((shm_id = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0660)) <= 0) {
         printf("Shared memory get faild.\n");
@@ -55,51 +59,65 @@ int main(int argc, const char **argv)
     if ((shm_addr = shmat(shm_id, NULL, 0)) == (void *) -1) {
         printf("Shared memory mapping faild. %d, [%s]\n",
                errno, strerror(errno));
-        return ERR;
+        status = ERR;
+        goto end;
     }
     memset(shm_addr, 0x00, sizeof(int));
     /* Shared memory unmap */
     if (shmdt(shm_addr) < 0) {
         printf("Shared memory unmapping faild.\n");
-        return ERR;
+        status = ERR;
+        goto end;
     }
+    shm_addr = NULL;
 
     pid_t child_pids[PROC_MAX];
     memset(child_pids, 0x00, sizeof(pid_t) * PROC_MAX);
 
     printf("Proc max: %d\n", PROC_MAX);
-    int is_parent = TRUE;
     for (i = 0; i < PROC_MAX; i++) {
         printf("Proc count: %d\n", i);
 
         if ((pid = fork()) == 0) {
             /* Child process start */
-            is_parent = FALSE;
             printf("Child process. My pid is %d, my parent's pid is %d.\n",
                    getpid(), getppid());
             do_something(shm_id);
             /* Child process end */
             break;
-        } else {
+        } else if (pid > 0) {
             printf("Parent process. My pid is %d, my child's pid is %d.\n",
                    getpid(), pid);
             child_pids[i] = pid;
+        } else if (pid == -1) {
+            printf("Parent process. My pid is %d. Have an error.\n", getpid());
+            status = ERR;
+            goto end;
+        } else {
+            printf("Fork faild. %d, [%s]\n",
+                   errno, strerror(errno));
+            status = ERR;
+            goto end;
         }
     }
 
+    int is_parent = (pid > 0);
     if (is_parent) {
-        int child_pid = 0;
-        int child_pid_status = 0;
-        pid_t returned_pid = 0;
+        int child_proc_status = 0;
         for (i = 0; i < PROC_MAX; i++) {
-            child_pid = child_pids[i];
-            if ((returned_pid =
-                 waitpid(child_pid, &child_pid_status, 0)) < 0) {
-                printf("waitpid failed.\n");
-                return ERR;
+            if (wait(&child_proc_status) < 0) {
+                printf("wait failed.\n");
+                status = ERR;
+                goto end;
             }
         }
     }
+end:
+    if (shm_addr) {
+        if (shmdt(shm_addr) < 0) {
+            printf("Shared memory unmapping faild.\n");
+        }
+    }
 
-    return EXIT_SUCCESS;
+    return (status == OK) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
